3c83d3e56052811fc32340cbd92d8c7eda2b5108
q <kanevagf@gmail.com>
2026-02-19 17:20:06 +0300

    Create __init__.py

---
+++ scr/gui/widgets/__init__.py
@@ -0,0 +1,6 @@
+# Reusable GUI widgets
+from src.gui.widgets.password_entry import PasswordEntry
+from src.gui.widgets.secure_table import SecureTable
+from src.gui.widgets.audit_log_viewer import AuditLogViewer
+
+__all__ = ["PasswordEntry", "SecureTable", "AuditLogViewer"]
