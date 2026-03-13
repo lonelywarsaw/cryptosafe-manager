95012c2de736c8e74f8076ea9f80b819784c488b
q <kanevagf@gmail.com>
2026-03-13 07:49:04 +0300

    Update abstract.py

--- scr/core/crypto/abstract.py
+++ scr/core/crypto/abstract.py
@@ -2,11 +2,11 @@
 
 
 class EncryptionService:
-    # интерфейс: шифрование и расшифрование получают key_manager и берут ключ из него (спринт 2)
+    # интерфейс: шифрование и расшифрование получают key_manager и берут ключ из него
     def encrypt(self, data: bytes, key_manager) -> bytes:
-        # данные шифруются ключом из key_manager.get_encryption_key() (спринт 2)
+        # данные шифруются ключом из key_manager.get_encryption_key()
         raise NotImplementedError
 
     def decrypt(self, ciphertext: bytes, key_manager) -> bytes:
-        # шифротекст расшифровывается тем же ключом через key_manager (спринт 2)
+        # шифротекст расшифровывается тем же ключом через key_manager
         raise NotImplementedError
